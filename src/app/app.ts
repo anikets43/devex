c451eff943bbd7810769edd6da2655de564cf71c
Aniket <anikets43@gmail.com>
2025-11-29 14:55:55 +0530

    chore: add devextreme through devextreme-cli

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { DxDataGridModule } from 'devextreme-angular';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxDataGridModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  data = new Array(100).fill(0).map((_, index) => ({
+    id: index,
+    name: `Item ${index}`,
+    description: `Description ${index}`,
+  }));
+  
   protected readonly title = signal('devex');
 }
